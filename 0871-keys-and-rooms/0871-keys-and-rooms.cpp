40ed43c04f2dc7244dfafe2dd9476750f296cdf0
Tejas Srikanth <65552202+tejas-srikanth@users.noreply.github.com>
2023-08-16 15:53:23 -0400

    Time: 3 ms (98.93%), Space: 10.5 MB (30.56%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.cpp
+++ 0871-keys-and-rooms/0871-keys-and-rooms.cpp
@@ -1,39 +1,22 @@
 class Solution {
 public:
 
-    void dfs(unordered_map<int, vector<int>>& g, vector<bool>& v, int start){
+    void dfs(vector<vector<int>>& rooms, vector<bool>& v, int start){
         v[start] = true;
-        for (int c: g[start]){
+        for (int c: rooms[start]){
             if (!v[c]){
-                dfs(g, v, c);
+                dfs(rooms, v, c);
             }
         }
     }
 
 
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        unordered_map<int, vector<int>> fg;
-        unordered_map<int, vector<int>> bg;
         vector<bool> fv(rooms.size(), false);
-        vector<bool> bv(rooms.size(), false);
-
-        for (int i=0; i<rooms.size(); ++i){
-            fg[i] = rooms[i];
-            for (int k: rooms[i]){
-                if (bg.find(k) != bg.end()){
-                bg[k].push_back(i);
-                } else {
-                    bg[k] = {i};
-                }
-            }
-        }
-        for (bool c: fv){ cout << c << " "; }
-        dfs(fg, fv, 0);
+        dfs(rooms, fv, 0);
     
-        
-        dfs(bg, bv, 0);
         for (int i=0; i<fv.size(); ++i){
-            if (!fv[i] && !bv[i]){
+            if (!fv[i]){
                 return false;
             } 
         }
