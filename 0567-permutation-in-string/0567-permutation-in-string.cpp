808c5b5216a3fcf70ee56ed4e3fb3115266f2ad6
Tejas Srikanth <65552202+tejas-srikanth@users.noreply.github.com>
2024-07-26 11:16:32 -0400

    Time: 59 ms (17.84%), Space: 8.9 MB (42.2%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    bool checkInclusion(string s1, string s2) {
+        vector<int> s1_arr(26);
+        vector<int> s2_arr(26);
+        for (char c: s1){
+            s1_arr[c - 'a']++;
+        }
+        if (s2.length() < s1.length()){
+            return false;
+        }
+
+        int winStart = 0;
+        int winEnd = 0;
+        for (int i=0; i<s1.length(); ++i){
+            cout << winEnd << endl;
+            char c = s2[winEnd];
+            s2_arr[c - 'a']++;
+            winEnd++;
+        }
+        winEnd--;
+        while (winEnd < s2.length()-1){
+            cout << winStart << " " << winEnd << endl;
+            if (s1_arr == s2_arr){
+                return true;
+            } else {
+                s2_arr[s2[winStart] - 'a']--;
+                winStart++;
+                winEnd++;
+                s2_arr[s2[winEnd] - 'a']++;
+            }
+        }
+        return s1_arr == s2_arr;
+    }
+};
