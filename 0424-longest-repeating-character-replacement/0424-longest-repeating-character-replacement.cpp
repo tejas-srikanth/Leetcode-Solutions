a43e46943b637ab82f66149ae4e958c4fe1704e1
Tejas Srikanth <65552202+tejas-srikanth@users.noreply.github.com>
2024-07-26 10:58:10 -0400

    Time: 110 ms (6.54%), Space: 37 MB (5.11%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int mxCount(vector<int> x){
+        int currMax = -1;
+        int tot = 0;
+        for (int j: x){
+            tot += j;
+            currMax = max(j, currMax);
+        }
+        return tot - currMax;
+    }
+    int characterReplacement(string s, int k) {
+        int st = 0;
+        int ed = 0;
+        vector<int> charArr(26, 0);
+        charArr[s[0] - 'A']++;
+        int longestWindow = 1;
+        while (ed < s.length()-1){
+            cout << s[ed] << " " << s[st] << endl;
+            ed++;
+            charArr[s[ed] - 'A']++;
+            if (mxCount(charArr) > k){
+                
+                charArr[s[st] - 'A']--;
+                st++;
+            }
+            else {
+                longestWindow = max(ed - st + 1, longestWindow);
+            }
+            
+        }
+        return longestWindow;
+    }
+};
