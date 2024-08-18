e5e1af3333c71850ee0c1f1af600b59fd1b3a432
Tejas Srikanth <65552202+tejas-srikanth@users.noreply.github.com>
2024-08-17 20:51:49 -0400

    Time: 2 ms (45.46%), Space: 11.9 MB (25.98%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -14,12 +14,12 @@ public:
     TreeNode* invertTree(TreeNode* root) {
         if (!root){
             return nullptr;
+        } else {
+            TreeNode* left = invertTree(root->left);
+            TreeNode* right = invertTree(root->right);
+            root->left = right;
+            root->right = left;
+            return root;
         }
-        TreeNode* tmp = root->right;
-        root->right = root->left;
-        root->left = tmp;
-        root->left = invertTree(root->left);
-        root->right = invertTree(root->right);
-        return root;
     }
 };
