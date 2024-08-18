80aca276895062bd96bbb121f0c91dfd3f5b9598
Tejas Srikanth <65552202+tejas-srikanth@users.noreply.github.com>
2024-08-18 09:52:58 -0400

    Time: 2 ms (43.9%), Space: 12 MB (47.41%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -14,12 +14,12 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if (!p && !q){
             return true;
-        } else if (!p && q){
+        }
+        else if (!(p && q)){
             return false;
-        } else if (p && !q){
-            return false;
-        } else {
-            return (p->val == q->val) && isSameTree(p->right, q->right) && isSameTree(p->left, q->left);
+        }
+        else {
+            return (p->val == q->val) && (isSameTree(p->right, q->right)) && (isSameTree(p->left, q->left));
         }
     }
 };
