fe551bc7c4d0fbb09c76ece283dc5d75aad9fcb4
Tejas Srikanth <65552202+tejas-srikanth@users.noreply.github.com>
2025-01-16 20:06:47 -0500

    Time: 134 ms (24.82%), Space: 22.4 MB (24.82%) - LeetHub

---
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.py
@@ -0,0 +1,8 @@
+class Solution:
+    def doesValidArrayExist(self, derived: List[int]) -> bool:
+        firstEl = 0
+        lastEl = 0
+        for i in range(1, len(derived) + 1):
+            theIndex = i % len(derived)
+            lastEl = lastEl ^ derived[theIndex]
+        return lastEl == 0
