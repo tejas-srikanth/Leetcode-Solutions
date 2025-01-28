90479e44ccb56af16bc4ed50b761182e634aea33
Tejas Srikanth <65552202+tejas-srikanth@users.noreply.github.com>
2025-01-28 14:56:15 -0500

    Time: 0 ms (100%), Space: 17.4 MB (28.05%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -6,13 +6,14 @@ public:
             if (visited[i] == -1){
                 queue<pair<int, int>> q;
                 q.push({i, 0});
+                visited[i] = 0;
                 while (q.size()){
                     int a = q.front().first, b = q.front().second;
                     q.pop();
-                    visited[a] = b;
                     for (int n: graph[a]){
                         if (visited[n] == -1){
                             q.push({n, 1 - b});
+                            visited[n] = 1 - b;
                         } else if (visited[n] == b) {
                             return false;
                         }
