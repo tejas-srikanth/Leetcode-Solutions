059dbd7bb038f2c81e64c967bac9f7c5b8441711
Tejas Srikanth <65552202+tejas-srikanth@users.noreply.github.com>
2025-01-31 10:07:33 -0500

    Time: 0 ms (100%), Space: 29.3 MB (7.15%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -11,31 +11,30 @@
  */
 class Solution {
 public:
-    pair<int, int> helper(TreeNode* root){
+    pair<int, int> maxPathHelper(TreeNode* root){
         if (!root){
             return {0, 0};
         } else if (!root->left && !root->right){
             return {root->val, root->val};
-        } else if (!root->left){
-            pair<int, int> p2 = helper(root->right);
-            int longestPathSum = max(p2.first + root->val, root->val);
-            return {longestPathSum, max({p2.second, p2.first + root->val, root->val, p2.first})};
         } else if (!root->right){
-            pair<int, int> p2 = helper(root->left);
-            int longestPathSum = max(p2.first + root->val, root->val);
-            return {longestPathSum, max({p2.second, p2.first + root->val, root->val, p2.first})};
-        }
-        else {
-            pair<int, int> p1 = helper(root->left);
-            pair<int, int> p2 = helper(root->right);
-            int longestPathSum = max(max(p1.first, p2.first) + root->val, root->val);
-            return {longestPathSum, max({p1.second, p2.second, p1.first + p2.first + root->val, p1.first + root->val, p2.first + root->val, root->val, p1.first, p2.first})};
+            pair<int, int> leftRes = maxPathHelper(root->left);
+            int maxPathOverall = max({leftRes.first, leftRes.second + root->val, root->val});
+            int maxLine = max({leftRes.second + root->val, root->val});
+            return {maxPathOverall, maxLine};
+        } else if (!root->left){
+            pair<int, int> leftRes = maxPathHelper(root->right);
+            int maxPathOverall = max({leftRes.first, leftRes.second + root->val, root->val});
+            int maxLine = max({leftRes.second + root->val, root->val});
+            return {maxPathOverall, maxLine};
+        } else {
+            pair<int, int> leftRes = maxPathHelper(root->left);
+            pair<int, int> rightRes = maxPathHelper(root->right);
+            int maxPathOverall = max({leftRes.first, leftRes.second + root->val, root->val, rightRes.first, rightRes.second + root->val, leftRes.second + rightRes.second + root->val});
+            int maxLine = max({leftRes.second + root->val, rightRes.second + root->val, root->val});
+            return {maxPathOverall, maxLine};
         }
     }
     int maxPathSum(TreeNode* root) {
-        if (!root){
-            return 0;
-        }
-        return helper(root).second;
+        return maxPathHelper(root).first;
     }
 };
