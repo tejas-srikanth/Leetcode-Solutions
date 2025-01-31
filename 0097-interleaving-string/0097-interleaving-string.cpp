4af41acb1c0f9615d2bcc71a051e7c235ac8ff2c
Tejas Srikanth <65552202+tejas-srikanth@users.noreply.github.com>
2025-01-31 11:23:56 -0500

    Time: 0 ms (100%), Space: 9.3 MB (66.52%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,59 +1,26 @@
 class Solution {
 public:
-
-    void solve(vector<vector<vector<bool>>>& dp, string s1, string s2, string s3){
-        int s1len = s1.length();
-        int s2len = s2.length();
-        int s3len = s3.length();
-        for (int i=0; i<=s1len; ++i){
-            for (int j=0; j<=s2len; ++j){
-                for (int k=0; k<=s3len; ++k){
-                    if (k == 0){
-                        if (i == 0 && j == 0){
-                            dp[i][j][k] = 1;
-                        } else {
-                            dp[i][j][k] = 0;
-                        }
-                    } else if (i == 0){
-                        if (j == 0){
-                            dp[i][j][k] = 0;
-                        } else {
-                            dp[i][j][k] = (dp[i][j-1][k-1] && s2[j-1] == s3[k-1]);
-                        }
-                    } else if (j == 0){
-                        dp[i][j][k] = (dp[i-1][j][k-1] && s1[i-1] == s3[k-1]);
-                    } else {
-                        if (s1[i-1] == s3[k-1]){
-                            if (s2[j-1] == s3[k-1]){
-                                dp[i][j][k] = (dp[i-1][j][k-1] || dp[i][j-1][k-1]);
-                            } else {
-                                dp[i][j][k] = dp[i-1][j][k-1];
-                            }
-                        } else if (s2[j-1] == s3[k-1]) {
-                            dp[i][j][k] = dp[i][j-1][k-1];
-                        } else {
-                            dp[i][j][k] = 0;
-                        }
-                    }
-                }
-            }
-        }
-
-    }
     bool isInterleave(string s1, string s2, string s3) {
-        vector<vector<vector<bool>>> dp;
-        for (int i=0; i<=s1.length(); ++i){
-            vector<vector<bool>> m;
-            for (int j=0; j<=s2.length(); ++j){
-                vector<bool> x;
-                for (int k=0; k<=s3.length(); ++k){
-                    x.push_back(false);
-                }
-                m.push_back(x);
-            }
-            dp.push_back(m);
+        if (s1.length() + s2.length() != s3.length()){
+            return false;
         }
-        solve(dp, s1, s2, s3);
-        return dp[s1.length()][s2.length()][s3.length()];
+        vector<vector<bool>> dp(s1.size() + 1, vector<bool>(s2.size() + 1));
+        for (int i=0; i<=s2.size(); ++i){
+            if (i == 0){
+                dp[0][0] = true;
+            } else {
+                dp[0][i] = (dp[0][i-1] && s2[i-1] == s3[i-1]);
+            }
+        }
+        for (int j=1; j<=s1.size(); ++j){
+            dp[j][0] = (dp[j-1][0] && s1[j-1] == s3[j-1]);
+        }
+        for (int i=1; i<=s1.size(); ++i){
+            for (int j=1; j<=s2.size(); ++j){
+                int s3Ind = i + j - 1;
+                dp[i][j] = ((dp[i][j-1] && s3[s3Ind] == s2[j-1]) || (dp[i-1][j] && s3[s3Ind] == s1[i-1])); 
+            }
+        }
+        return dp.back().back();
     }
 };
