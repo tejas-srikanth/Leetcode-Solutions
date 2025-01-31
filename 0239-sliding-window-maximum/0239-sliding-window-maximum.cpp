e1ecf6a393cb67a7e1553e77930bd76f30c6bca6
Tejas Srikanth <65552202+tejas-srikanth@users.noreply.github.com>
2025-01-31 09:41:36 -0500

    Time: 408 ms (5.38%), Space: 216.3 MB (5.77%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
-
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> dq;
-        vector<int> res;
-        int winStart = 0;
-        for (int i=0; i<nums.size(); ++i){
-            if (i < k){
-                if (dq.empty()){
-                    dq.push_back(nums[i]);
-                } else  {
-                    while (!dq.empty() && dq.front() < nums[i]){
-                        dq.pop_front();
-                    }
-                    dq.push_front(nums[i]);
-                }
-            } else {
-                res.push_back(dq.back());
-                if (dq.back() == nums[winStart]){
-                    dq.pop_back();
-                }
-                while (!dq.empty() && dq.front() < nums[i]){
-                        dq.pop_front();
-                    }
-                dq.push_front(nums[i]);
-                winStart++;
-            }  
+        set<pair<int, int>> currWindow;
+        for (int i=0; i<k; ++i){
+            currWindow.insert({nums[i], i});
         }
-        res.push_back(dq.back());
+        vector<int> res;
+        for (int i=k; i<nums.size(); ++i){
+            res.push_back(prev(currWindow.end())->first);
+            currWindow.erase(currWindow.find({nums[i-k], i-k}));
+            currWindow.insert({nums[i], i});
+        }
+        res.push_back(prev(currWindow.end())->first);
         return res;
     }
 };
