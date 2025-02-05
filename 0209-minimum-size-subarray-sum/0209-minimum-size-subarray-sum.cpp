5e889873680a74be503056b40ec779d0e483883a
Tejas Srikanth <65552202+tejas-srikanth@users.noreply.github.com>
2025-02-05 12:06:01 -0500

    Time: 854 ms (5.03%), Space: 34.2 MB (5.02%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        vector<int> prefArray = {0};
+        for (int i=0; i<nums.size(); ++i){
+            prefArray.push_back(prefArray.back() + nums[i]);
+        }
+        int winStart = 0;
+        int winEnd = 0;
+        int best = nums.size() + 1;
+        if (std::accumulate(nums.begin(), nums.end(), 0) < target){
+            return 0;
+        }
+        while (winEnd < nums.size() + 1){
+            int currWindowSum = prefArray[winEnd] - prefArray[winStart];
+            cout << winStart << " " << winEnd << endl;
+            if (currWindowSum < target){
+                winEnd++;
+            } else {
+                
+                best = min(best, winEnd - winStart);
+                winStart++;
+            }
+        }
+        return best;
+    }
+};
