5ef4f85ae1286dae5146e29f5e49120dcb7d013d
Tejas Srikanth <65552202+tejas-srikanth@users.noreply.github.com>
2025-03-09 15:19:41 -0400

    Time: 0 ms (100%), Space: 7.8 MB (63.5%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        long long x = (long long)n;
-        return (long long)(1 + 2 * x * (x - 1));
+        return (1 + 2 * (long long)n * (n - 1));
     }
 };
