1564bc87bb1c868344d8c74eb5baceb2d77618ac
Tejas Srikanth <65552202+tejas-srikanth@users.noreply.github.com>
2025-03-31 20:52:42 -0400

    Time: 95 ms (56.53%), Space: 60.9 MB (50.81%) - LeetHub

---
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        dp = [0 for i in questions]
+        dp[-1] = questions[-1][0]
+        for i in range(len(dp)-2, -1, -1):
+            q = questions[i]
+            p, b = q[0], q[1]
+            if i + b + 1 >= len(dp):
+                dp[i] = max(p, dp[i + 1])
+            else:
+                dp[i] = max(p + dp[i + b + 1], dp[i + 1])
+        print(dp)
+        return dp[0]
+        
