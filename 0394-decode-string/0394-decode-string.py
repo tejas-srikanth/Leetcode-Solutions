355e9c04e90ff1fd28c498a02c48d843f30538b0
Tejas Srikanth <65552202+tejas-srikanth@users.noreply.github.com>
2025-04-04 14:20:37 -0400

    Time: 0 ms (100%), Space: 17.9 MB (34.92%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,41 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        s = "1[" + s + "]"
+        tokenized = []
+        currNum = 0
+        currString = ""
+        for i in range(len(s)):
+            if s[i].isdigit():
+                currNum *= 10
+                currNum += int(s[i])
+                if currString != "":
+                    tokenized.append(currString)
+                    currString = ""
+            elif s[i] == "[":
+                tokenized.append(currNum)
+                tokenized.append(s[i])
+                currNum = 0
+            elif s[i] == "]":
+                if currString != "":
+                    tokenized.append(currString)
+                    currString = ""
+                tokenized.append(s[i])
+            else:
+                currString += s[i]
+        currStack = []
+        print(tokenized)
+        for i in range(len(tokenized)):
+            if isinstance(tokenized[i], str) and tokenized[i] not in ["[", "]"]:
+                currStack.append(tokenized[i])
+            elif tokenized[i] == "[":
+                currStack.append(tokenized[i])
+            elif tokenized[i] == "]":
+                currString = ""
+                while currStack[-1] != "[":
+                    currString = currStack.pop() + currString
+                currStack.pop()
+                numRepeat = currStack.pop()
+                currStack.append(numRepeat * currString)
+            else:
+                currStack.append(tokenized[i])
+        return currStack[0]
